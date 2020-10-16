6fc936a39317f6184b5d4b05e24039525f892ac6
Vitor Guilherme Moreira Pedro <vitor.guilherme@concert.com.br>
2020-10-16 10:19:33 -0300

    final da aula #4

---
+++ hello-typescript/main.ts
@@ -0,0 +1,25 @@
+var minhaVar = 'minha variavel';
+
+function minhaFunc(x, y) {
+    return x + y;
+}
+
+let num = 2;
+const PI = 3.14;
+
+var numeros = [1, 2, 3];
+
+numeros.map(function (valor) {
+    return valor * 2;
+});
+
+numeros.map(valor => valor * 2); //ES 2015
+
+class Matematica {
+    soma(x, y) {
+        return x + y;
+    }
+}
+
+var n1: any = 'adfljsdf';
+n1 = 4;
