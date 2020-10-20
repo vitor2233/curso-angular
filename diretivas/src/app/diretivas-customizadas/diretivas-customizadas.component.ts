e1c34a5c204095b5d5c6f6e65d84ca75953723c4
Vitor Guilherme Moreira Pedro <vitor.guilherme@concert.com.br>
2020-10-20 11:56:42 -0300

    #36: ngElse

--- diretivas/src/app/diretivas-customizadas/diretivas-customizadas.component.ts
+++ diretivas/src/app/diretivas-customizadas/diretivas-customizadas.component.ts
@@ -7,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DiretivasCustomizadasComponent implements OnInit {
 
+  mostrarCursos: boolean = true;
+
+  onMostrarCursos(){
+    this.mostrarCursos = !this.mostrarCursos;
+  }
+
   constructor() { }
 
   ngOnInit(): void {
