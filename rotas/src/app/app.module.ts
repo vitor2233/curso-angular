8c1373c73b8d68917d4a01f38bbfc9eacfa85d4f
Vitor Guilherme Moreira Pedro <vitor.guilherme@concert.com.br>
2020-10-21 11:26:29 -0300

    #51: Rotas: RouterLink

--- rotas/src/app/app.module.ts
+++ rotas/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { CursosComponent } from './cursos/cursos.component';
 import { routing } from './app.routing';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { routing } from './app.routing';
   ],
   imports: [
     BrowserModule,
-    routing
+    routing,
+    NoopAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
