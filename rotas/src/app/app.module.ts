4804ba4c232f401eda07614b0b083d10f19cb82c
Vitor Guilherme Moreira Pedro <vitor.guilherme@concert.com.br>
2020-10-21 11:00:06 -0300

    #50: Rotas: Configurando rotas simples

--- rotas/src/app/app.module.ts
+++ rotas/src/app/app.module.ts
@@ -2,13 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { CursosComponent } from './cursos/cursos.component';
+import { routing } from './app.routing';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    CursosComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    routing
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ rotas/src/app/app.routing.ts
@@ -0,0 +1,15 @@
+import { RouterModule, Routes } from "@angular/router";
+import { ModuleWithProviders } from '@angular/core';
+
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { CursosComponent } from './cursos/cursos.component';
+
+const APP_ROUTES: Routes = [
+
+    { path: 'cursos', component: CursosComponent },
+    { path: 'login', component: LoginComponent },
+    { path: '', component: HomeComponent }
+];
+
+export const routing: ModuleWithProviders<any> = RouterModule.forRoot(APP_ROUTES);
