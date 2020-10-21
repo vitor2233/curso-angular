0f0e89bed1278547d71c3105762ae8957e79c13d
Vitor Guilherme Moreira Pedro <vitor.guilherme@concert.com.br>
2020-10-21 13:39:46 -0300

    #56: Rotas: Parâmetros de url (query)

--- rotas/src/app/cursos/cursos.component.ts
+++ rotas/src/app/cursos/cursos.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+
 import { CursosService } from './cursos.service';
 
 @Component({
@@ -9,11 +12,29 @@ import { CursosService } from './cursos.service';
 export class CursosComponent implements OnInit {
 
   cursos: any[];
+  pagina: number;
+  inscricao: Subscription;
 
-  constructor(private cursosService: CursosService) { }
+  constructor(private cursosService: CursosService,
+    private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit(): void {
     this.cursos = this.cursosService.getCursos();
+
+    this.inscricao = this.route.queryParams.subscribe(
+      (query: any) => {
+        this.pagina = query['pagina'];
+      }
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.inscricao.unsubscribe();
+  }
+
+  proximaPagina(){
+    this.router.navigate(['/cursos'],
+      {queryParams: { 'pagina': ++this.pagina}});
   }
 
 }
