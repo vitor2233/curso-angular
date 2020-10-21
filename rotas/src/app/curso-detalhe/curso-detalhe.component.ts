b3d3ee965757275f5ae8a635dca9ae669a3db924
Vitor Guilherme Moreira Pedro <vitor.guilherme@concert.com.br>
2020-10-21 12:39:41 -0300

    #54: Rotas:  Mudanças nos parâmetros de roteamento

--- rotas/src/app/curso-detalhe/curso-detalhe.component.ts
+++ rotas/src/app/curso-detalhe/curso-detalhe.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-curso-detalhe',
@@ -9,13 +10,22 @@ import { ActivatedRoute } from '@angular/router';
 export class CursoDetalheComponent implements OnInit {
 
   id: string;
+  inscricao: Subscription;
 
-  constructor(private route: ActivatedRoute) { 
-    this.id = this.route.snapshot.params['id'];
-    
+  constructor(private route: ActivatedRoute) {
+    //this.id = this.route.snapshot.params['id'];
   }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.inscricao = this.route.params.subscribe(
+      (params: any) => {
+        this.id = params['id'];
+      }
+    );
+  }
+
+  ngOnDestroy() {
+    this.inscricao.unsubscribe();
   }
 
 }
