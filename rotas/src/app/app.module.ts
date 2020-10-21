29c7ba75bea31fd13dce4cc874a226d543fd48fe
Vitor Guilherme Moreira Pedro <vitor.guilherme@concert.com.br>
2020-10-21 11:54:11 -0300

    #53: Rotas: Definindo e extraindo parâmetros de roteamento

--- rotas/src/app/app.module.ts
+++ rotas/src/app/app.module.ts
@@ -1,24 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { CursosComponent } from './cursos/cursos.component';
 import { routing } from './app.routing';
-import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { CursoDetalheComponent } from './curso-detalhe/curso-detalhe.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     LoginComponent,
-    CursosComponent
+    CursosComponent,
+    CursoDetalheComponent
   ],
   imports: [
     BrowserModule,
     routing,
-    NoopAnimationsModule
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
