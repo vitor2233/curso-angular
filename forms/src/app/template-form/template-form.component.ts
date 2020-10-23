0af84867198ad1290eb3debf63caf6ec92c30646
Vitor Guilherme Moreira Pedro <vitor.guilherme@concert.com.br>
2020-10-23 12:18:34 -0300

    #75: template driven: Inicializando valores com ngModel

--- forms/src/app/template-form/template-form.component.ts
+++ forms/src/app/template-form/template-form.component.ts
@@ -7,8 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TemplateFormComponent implements OnInit {
 
+  usuario: any = {
+    nome: 'Vitor',
+    email: 'vitor@email.com'
+  }
+
   onSubmit(form){
-    console.log(form);
+    console.log(form, this.usuario);
     
   }
 
