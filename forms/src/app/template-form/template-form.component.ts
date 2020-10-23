1dd4846b94a9ced307bfd79839373e85735870b1
Vitor Guilherme Moreira Pedro <vitor.guilherme@concert.com.br>
2020-10-23 11:57:44 -0300

    #74: Ttemplate driven: Controles ngForm, ngSubmit e ngModel

--- forms/src/app/template-form/template-form.component.ts
+++ forms/src/app/template-form/template-form.component.ts
@@ -7,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TemplateFormComponent implements OnInit {
 
+  onSubmit(form){
+    console.log(form);
+    
+  }
+
   constructor() { }
 
   ngOnInit(): void {
