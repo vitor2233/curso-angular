d30f389a1267f20fa8883eb91e125f4c3a0a8643
Vitor Guilherme Moreira Pedro <vitor.guilherme@concert.com.br>
2020-10-23 13:09:36 -0300

    #78: template driven: Aplicando CSS na validação dos campos

--- forms/src/app/template-form/template-form.component.ts
+++ forms/src/app/template-form/template-form.component.ts
@@ -8,8 +8,8 @@ import { Component, OnInit } from '@angular/core';
 export class TemplateFormComponent implements OnInit {
 
   usuario: any = {
-    nome: 'Vitor',
-    email: 'vitor@email.com'
+    nome: null,
+    email: null
   }
 
   onSubmit(form){
