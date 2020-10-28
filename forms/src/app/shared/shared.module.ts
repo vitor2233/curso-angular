86359ef4c2dd0309b81f9bab23bc982aac99d797
Vitor Guilherme Moreira Pedro <vitor.guilherme@concert.com.br>
2020-10-28 11:49:47 -0300

    #114: Formulários reativos: Campo input customizado (ControlValueAcessor)

--- forms/src/app/shared/shared.module.ts
+++ forms/src/app/shared/shared.module.ts
@@ -1,19 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 import { FormDebugComponent } from './form-debug/form-debug.component';
 import { CampoControlComponent } from './campo-control/campo-control.component';
 import { DropdownService } from './services/dropdown.service';
 import { HttpClientModule } from '@angular/common/http';
 import { ErrorMsgComponent } from './error-msg/error-msg.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-
-
+import { InputFieldComponent } from './input-field/input-field.component';
 
 @NgModule({
   declarations: [
     FormDebugComponent,
     CampoControlComponent,
-    ErrorMsgComponent
+    ErrorMsgComponent,
+    InputFieldComponent
   ],
   imports: [
     CommonModule,
@@ -24,7 +25,8 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
   exports: [
     FormDebugComponent,
     CampoControlComponent,
-    ErrorMsgComponent
+    ErrorMsgComponent,
+    InputFieldComponent
   ], providers: [DropdownService]
 })
 export class SharedModule { }
